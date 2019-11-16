cb7600c58c8d172752fa039408816f618515055e
linly <linlycode@gmail.com>
2019-11-16 11:40:22 +0800

    [math] Projection matrices

--- math/trans_mat.h
+++ math/trans_mat.h
@@ -37,4 +37,46 @@ Matrix<T, 4, 4> scale(const Vector<T, 3> &s) {
 	return a;
 }
 
+template <typename T>
+Matrix<T, 4, 4> ortho_projection(
+	T left, T right, T bottom, T top, T near, T far) {
+
+	T a[4][4] = {
+		{2 / (right - left), 0, 0, -(right + left) / (right - left)},
+		{0, 2 / (top - bottom), 0, -(top + bottom) / (top - bottom)},
+		{0, 0, -2 / (far - near), -(far + near) / (far - near)},
+		{0, 0, 0, 1},
+	};
+
+	return a;
+}
+
+template <typename T>
+Matrix<T, 4, 4> perspective_projection(
+	T left, T right, T bottom, T top, T near, T far) {
+
+	T a[4][4] = {
+		{2 * near / (right - left), 0, (right + left) / (right - left), 0},
+		{0, 2 * near / (top - bottom), (top + bottom) / (top - bottom), 0},
+		{0, 0, -(far + near) / (far - near), -2 * far * near / (far - near)},
+		{0, 0, -1, 0},
+	};
+
+	return a;
+}
+
+template <typename T>
+Matrix<T, 4, 4> perspective_projection(T fovy, T aspect, T near, T far) {
+	T tan = std::tan(fovy / 2);
+
+	T a[4][4] = {
+		{1 / (aspect * tan), 0, 0, 0},
+		{0, 1 / tan, 0, 0},
+		{0, 0, -(far + near) / (far - near), -2 * far * near / (far - near)},
+		{0, 0, -1, 0},
+	};
+
+	return a;
+}
+
 #endif
